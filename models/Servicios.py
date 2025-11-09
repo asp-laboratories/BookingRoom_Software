c6103cede5b5d1d10b3e2740fb53cb02aafe9d3a
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-11-09 15:18:40 -0800

    feat(models): Creacion de la clase servicio (Update)

---
+++ models/Servicios.py
@@ -0,0 +1,41 @@
+class Servicio:
+    def __init__(self, codigo, nombre, costo_renta, tipo_servicio):
+        self.__codigo = codigo
+        self.__nombre = nombre
+        self.__costo_renta = costo_renta
+        self.__tipo_servicio = tipo_servicio
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre
+
+    @property
+    def tipo_servicio(self):
+        return self.__tipo_servicio
+
+    @tipo_servicio.setter
+    def tipo_servicio(self, tipo_servicio):
+        self.__tipo_servicio = tipo_servicio
+
+    
+
+class TipoServicio:
+    def __init__(self, codigo, descripcion):
+        self.__codigo = codigo
+        self.__descripcion = descripcion
+
+    @property
+    def descripcion(self):
+        return self.__descripcion
+
+    @descripcion.setter
+    def descripcion(self, descripcion):
+        self.__descripcion = descripcion
+
+
+        
+        
