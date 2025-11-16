39d50e9d8ae4462852dd1b635af98b3136120980
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-11-15 16:15:51 -0800

    fix(tests): Creacion de menus para una mejor interraccion para el apartado de servicios

--- tests/test_servicio.py
+++ tests/test_servicio.py
@@ -3,16 +3,45 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from services.ServicioServices import ServicioService
-   
-def main():
 
-    servicio_service = ServicioService()
-    eleccion_panel = int(input("--Panel servicio--\n1. Registrar servicio\n2. Listar servicios\nOpcion: "))
-    if eleccion_panel == 1:
-        servicio_service.registrar_servicio("SRVDC","Servicio de copiado", "Copias", 20.00, "SRPRN")
-    elif eleccion_panel == 2:
+servicio_service = ServicioService()
+def añadir_ser():
+    while True:
+        ciclo = int(input("¿Añadir servicio?\n1. Si \n2. No\nOpcion: "))
+        if ciclo == 1:
+            codigo = input("Codigo de servicio: ")
+            nombre = input("Nombre del servicio: ")
+            descripcion = input("descripcion del servicio: ")
+            costo = float(input("Costo del servicio: "))
+            tipo = input("Codigo del tipo de servicio")
+            servicio_service.registrar_servicio(codigo,nombre, descripcion, costo, tipo)
+        elif ciclo == 2:
+            break
+        else:
+            print("Esa opcion no existe, ingrese una opcion valida")
+
+
+def listar_ser():
+    while True:
         servicio_service.listar_servicio()
-    else:
-        print("No existe esa opcion")
+        break
+
+       
+def main():
+    while True:
+        eleccion_panel = int(input("--Panel servicio--\n1. Registrar servicio\n2. Listar servicios\n3. Salir \nOpcion: "))
+        if eleccion_panel == 1:
+            añadir_ser()
+        elif eleccion_panel == 2:
+            listar_ser()
+        elif eleccion_panel == 3:
+            break
+        else:
+            print("No existe esa opcion")
+
+
 if __name__ == "__main__":
     main()
+
+
+
--- tests/test_tipo_servicio.py
+++ tests/test_tipo_servicio.py
@@ -3,15 +3,44 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from services.TipoServicioService import TipoServicioService
-#from models.Servicios import Servicio, TipoServicio
-   
-def main():
-    service = TipoServicioService()
-    #service.registrar_tipo_servicio("SRPRN", "Servicios para reuniones")
-    #service.registrar_tipo_servicio("SREJC", "Servicios ejecutivos")
-    #service.registrar_tipo_servicio("SRIAV", "Acceso a internet de alta velocidad")
+service = TipoServicioService()
 
-    #service.listar_tipos_servicio()
-    service.mostrar_servicios_de_tipo("SRPRN")
+
+def registrar_tipo():
+    while True:
+        opcion = int(input("\n¿Agregar tipo de servicio?\n1. Si\n2. No\nopcion: "))
+        if opcion == 1:
+            codigo = input("Codigo de tipo:")
+            descripcion = input("Descripcion del tipo: ")
+            service.registrar_tipo_servicio(codigo, descripcion)
+        elif opcion == 2:
+            break
+        else:
+            print("Escribe una opcion correcta")
+
+def mostrar_tipo_con_ser():
+    while True:
+        opcion = int(input("\nMostrar los servicios de un tipo de servicio:\n1. Si \n2. No\nopcion: "))
+        if opcion == 1:
+            codigoTiSer = input("Codigo del tipo: ")
+            service.mostrar_servicios_de_tipo(codigoTiSer)
+        elif opcion == 2:
+            break 
+        else:
+            print("Vuelve a ingresar")
+def main():
+    while True:
+        opcion = int(input("\n--panel tipo de servicios--\n1.Agregar. \n2.Listar\n3.Listar un tipo con sus servicios\n4.Salir\n opcion: "))
+        if opcion == 1:
+            registrar_tipo()
+        elif opcion == 2:
+            service.listar_tipos_servicio()
+        elif opcion == 3:
+            mostrar_tipo_con_ser()
+        elif opcion == 4:
+            break
+        else:
+            print("No existe esa opcion ingresa de nuevo")
+            
 if __name__ == "__main__":
     main()
