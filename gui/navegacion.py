0a91a7ea1a27af94b687a9e793ee49a3ae85fd6e
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-11-22 21:57:31 -0800

    feat(Imagenes, gui): Primer diseño de figma hecho (login), funcionamiento de la interfaz de servicio, agregacion de mensaje de confirmacion aunque de error falta por programar (Update)

--- gui/navegacion.py
+++ gui/navegacion.py
@@ -2,16 +2,16 @@ import os
 from pathlib import Path
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMessageBox
-
-
+from services.ServicioServices import ServicioService
 ruta_ui = Path(__file__).parent / "navegacion.ui"
+servicio = ServicioService()
 
 class Navegacion():
     def __init__(self):
         self.navegacion = uic.loadUi(str(ruta_ui))
         # self.initGUI()
         self.navegacion.show()
-
+        self.navegacion.sMensaje.setText("")
         self.navegacion.subMenuAdministracion.setVisible(False)
         self.navegacion.subMenuAlmacen.setVisible(False)
         self.navegacion.subMenuRecepcion.setVisible(False)
@@ -23,6 +23,8 @@ class Navegacion():
         self.navegacion.servicios.clicked.connect(lambda: self.mostrar_pagina(1))
         self.navegacion.equipamiento.clicked.connect(lambda: self.mostrar_pagina(2))
 
+        self.navegacion.sConfirmar.clicked.connect(self.registar_servicio)
+
     def abrir_opciones_admin(self):
         self.navegacion.subMenuAdministracion.setVisible(not self.navegacion.subMenuAdministracion.isVisible())
     def abrir_opciones_almac(self):
@@ -30,8 +32,17 @@ class Navegacion():
     def abrir_opciones_recep(self):
         self.navegacion.subMenuRecepcion.setVisible(not self.navegacion.subMenuRecepcion.isVisible())
 
+    def registar_servicio(self):
+        resultado = servicio.registrar_servicio(self.navegacion.sNombreSer.text(), self.navegacion.sDescripcion.text(), self.navegacion.sCostoRenta.text(), self.navegacion.sTipoServicio.text())
+        if resultado == False:
+            self.navegacion.sMensaje.setText("Incorrecto")
+        else:
+            self.navegacion.sMensaje.setText("Correcto")
+
     def mostrar_pagina(self, indice):
         self.navegacion.stackedWidget.setCurrentIndex(indice)
+
+    
 
 
 
