282a1265833a470d7c8101e01ea54f2d0f578ada
jonathangdm <jjonathangdm@gmail.com>
2025-11-25 13:07:22 -0800

    feat(models): añadi property y setter en pagos y elimine datos corrompidos

---
+++ models/Pago.py
@@ -0,0 +1,74 @@
+class pago:
+    def __init__(self, num, montoPago, descripcion, fecha, hora, noPago, reservacion, metodo_pago, concepto_pago):
+        self.__num = num
+        self.__montoPago = montoPago
+        self.__descripcion = descripcion
+        self.__fecha = fecha
+        self.__hora = hora
+        self.__noPago = noPago
+        self.__reservacion = reservacion
+        self.__metodo_pago = metodo_pago
+        self.__concepto_pago = concepto_pago
+
+    @property
+    def num(self):
+        return self.__num
+    @num.setter
+    def num(self, num):
+        self.__num = num
+
+    @property
+    def montoPago(self):
+        return self.__montoPago
+    @montoPago.setter
+    def montoPago(self, montoPago):
+        self.__montoPago = montoPago
+
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    @descripcion.setter
+    def descripcion(self, descripcion):
+        self.__descripcion = descripcion
+
+    @property
+    def fecha(self):
+        return self.__fecha
+    @fecha.setter
+    def fecha(self, fecha):
+        self.__fecha = fecha
+
+    @property
+    def hora(self):
+        return self.__hora
+    @hora.setter
+    def hora(self, hora):
+        self.__hora = hora
+
+    @property
+    def noPago(self):
+        return self.__noPago
+    @noPago.setter
+    def noPago(self, noPago):
+        self.__noPago = noPago
+
+    @property
+    def reservacion(self):
+        return self.__reservacion
+    @reservacion.setter
+    def reservacion(self, reservacion):
+        self.__reservacion = reservacion
+
+    @property
+    def metodo_pago(self):
+        return self.__metodo_pago
+    @metodo_pago.setter
+    def metodo_pago(self, metodo_pago):
+        self.__metodo_pago = metodo_pago
+
+    @property
+    def concepto_pago(self):
+        return self.__concepto_pago
+    @concepto_pago.setter
+    def concepto_pago(self, concepto_pago):
+        self.__concepto_pago = concepto_pago
