2a3bf42b71bce6878359190fa416f86eb31a3bfb
LuisDavidGR <ldavidgrdrmz@gmail.com>
2025-11-24 22:19:36 -0800

    feat(gui): Seleccion del salon pero sin diseño, solo para probar (Update)

--- gui/navegacion.py
+++ gui/navegacion.py
@@ -4,10 +4,11 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QMessageBox
 from services.SalonServices import SalonServices
 from services.ServicioServices import ServicioService
+from services.EquipamentoService import EquipamentoService
 ruta_ui = Path(__file__).parent / "navegacion.ui"
 servicio = ServicioService()
 salon = SalonServices()  
-
+equipamiento = EquipamentoService()
 
 class Navegacion():
     def __init__(self):
@@ -16,6 +17,7 @@ class Navegacion():
         self.navegacion.show()
         self.navegacion.sMensaje.setText("")
         self.navegacion.saMensaje.setText("")
+        self.navegacion.eMensaje.setText("")
         self.navegacion.subMenuAdministracion.setVisible(False)
         self.navegacion.subMenuAlmacen.setVisible(False)
         self.navegacion.subMenuRecepcion.setVisible(False)
@@ -27,13 +29,17 @@ class Navegacion():
         self.navegacion.servicios.clicked.connect(lambda: self.mostrar_pagina(1))
         self.navegacion.equipamiento.clicked.connect(lambda: self.mostrar_pagina(2))
         self.navegacion.salon.clicked.connect(lambda: self.mostrar_pagina(3))
+        self.navegacion.reservacion.clicked.connect(lambda: self.mostrar_pagina(4))
 
         self.navegacion.sConfirmar.clicked.connect(self.registar_servicio)
 
         self.navegacion.saConfirmar.clicked.connect(self.registrar_salon)
         self.navegacion.saCancelar.clicked.connect(self.limpiar_salon)
+        
+        self.cargar_seleccion_salon()
+        self.navegacion.reSalonInfo.clicked.connect(self.mostrar_info_salon)
 
-
+        self.navegacion.eConfirmar.clicked.connect(self.registrar_equipamiento)
 
     def abrir_opciones_admin(self):
         self.navegacion.subMenuAdministracion.setVisible(not self.navegacion.subMenuAdministracion.isVisible())
@@ -61,6 +67,15 @@ class Navegacion():
         else:
             self.navegacion.saMensaje.setText("Correcto")
     
+    def registrar_equipamiento(self):
+        resultado = equipamiento.registrar_equipamento(self.navegacion.eNombreEqui.text(),self.navegacion.eDescripcion.text(), float(self.navegacion.eCostoRenta.text()), int(self.navegacion.eStock.text()), self.navegacion.eTipoEquipamiento.text())
+        if resultado == False:
+            self.navegacion.eMensaje.setText("Incorrecto")
+        else:
+            self.navegacion.eMensaje.setText("Correcto")
+
+
+
 
     def limpiar_salon(self):
         self.navegacion.saNombre.clear()
@@ -76,7 +91,28 @@ class Navegacion():
         self.navegacion.stackedWidget.setCurrentIndex(indice)
 
     
-
+    def cargar_seleccion_salon(self):
+        self.navegacion.reSalonSelecc.clear()
+        self.navegacion.reSalonSelecc.addItem("Selecciona un salon", None)
+        obtener = salon.listar_salones()
+        for sln in obtener:
+            self.navegacion.reSalonSelecc.addItem(sln["nombre"], sln["numSalon"])
+    
+    def mostrar_info_salon(self):
+        salNumero = self.navegacion.reSalonSelecc.currentData()
+        sali = self.buscar_usuario_por_id(salNumero)
+        mensaje = "INFORMACION DEL SALON"
+        mensaje += f"\n NOMBRE: {sali["nombre"]}"
+        mensaje += f"\n COSTO: {str(sali["costoRenta"])}"
+        if sali:
+            self.navegacion.resultadoSalon.setText(mensaje)
+    
+    def buscar_usuario_por_id(self, salNumero):
+        """Buscar usuario por ID en los datos de prueba"""
+        for s in salon.listar_salones():
+            if s["numSalon"] == salNumero:
+                return s
+        return None 
 
 
     # def initGUI(self):
